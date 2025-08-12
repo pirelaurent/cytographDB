25b1367f014c6fbc3cd41bd168e8b1c516c2bf1d
pireLaurent <pierre.laurent@pep-inno.com>
2025-08-12 21:46:51 +0200

    avoid to stop in case a declared custom module is missing

--- public/js/customModulesIndex.js
+++ public/js/customModulesIndex.js
@@ -4,11 +4,24 @@
     set them into public/custom  directory as this is out of the git upload 
     as a js module app, path must be relative : 
 */
-import '../custom/democytodb.js';
+// main.js (aucun import statique vers des modules optionnels !)
+const optionalModules = [
+  '../custom/democytodb.js',
+  '../custom/AWProject.js',
+  '../custom/fake.js',
+];
 
-// adding other specific modules
-import '../custom/AWProject.js';
-import '../custom/HSW.js';
+// on déclenche les imports sans attendre qu’ils finissent
+optionalModules.forEach(spec => {
+  import(spec).then(() => {
+    console.info('[custom] chargé :', spec);
+  }).catch(err => {
+    console.debug('[custom] ignoré :', spec, err?.message || err);
+  });
+});
+
+// la suite de ton appli part immédiatement
+//startApp(); // <-- rien n’est bloqué
 
 /*
     to avoid upload of modif in YOUR LOCAL REPO : 
